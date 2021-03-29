aafd62a8a3288dfa4e731d4669f7ac8602da679e
Satyarth Agrahari <satylogin@gmail.com>
2021-03-29 13:28:52 +0530

    adding request handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::Read;
 
 const ADDRESS: &str = "127.0.0.1:7878";
+
+const BUFFER_LEN: usize = 1024;
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; BUFFER_LEN];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
 
 fn main() {
     let listener = TcpListener::bind(ADDRESS).unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("Connection established! {:?}", stream);
+        let stream: TcpStream = stream.unwrap();
+        handle_connection(stream);
     }
 }
