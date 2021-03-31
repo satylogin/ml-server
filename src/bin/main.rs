9bf00c33a1d04f76f6b0ee40a37d402e4b002268
Satyarth Agrahari <satylogin@gmail.com>
2021-03-31 09:46:43 +0530

    adding threadpool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,22 @@
+use ml_server::ThreadPool;
 use std::fs;
 use std::io::Read;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 const ADDRESS: &str = "127.0.0.1:7878";
 
 const BUFFER_LEN: usize = 1024;
 
+const THREAD_POOL_SIZE: usize = 4;
+
 const LANDING_PAGE: &str = "data/index.html";
 const NOT_FOUND_PAGE: &str = "data/404.html";
 
-fn serve_landing_page() -> String {
+fn landing_page() -> String {
     let contents = fs::read_to_string(LANDING_PAGE).unwrap();
 
     format!(
@@ -32,34 +37,46 @@ fn unknown_request() -> String {
     format!("{}{}", status, contents)
 }
 
-fn get_formatted_request(path: &str) -> String {
-    format!("GET {} HTTP/1.1\r\n", path)
+fn long_running_request() -> String {
+    thread::sleep(Duration::from_secs(5));
+    landing_page()
+}
+
+fn check_request_path(buffer: [u8; BUFFER_LEN], path: &str) -> bool {
+    buffer.starts_with(format!("GET {} HTTP/1.1\r\n", path).as_bytes())
 }
 
 fn get_response(buffer: [u8; BUFFER_LEN]) -> String {
-    if buffer.starts_with(get_formatted_request("/ping").as_bytes()) {
+    if check_request_path(buffer, "/long") {
+        long_running_request()
+    } else if check_request_path(buffer, "/ping") {
         health_check()
-    } else if buffer.starts_with(get_formatted_request("/").as_bytes()) {
-        serve_landing_page()
+    } else if check_request_path(buffer, "/") {
+        landing_page()
     } else {
         unknown_request()
     }
 }
 
-fn route(mut stream: TcpStream) {
+fn handle(mut stream: TcpStream) {
     let mut buffer = [0; BUFFER_LEN];
     stream.read(&mut buffer).unwrap();
     println!("{}", String::from_utf8_lossy(&buffer[..]));
 
-    stream.write(get_response(buffer).as_bytes()).unwrap();
+    stream.write_all(get_response(buffer).as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
 fn main() {
     let listener = TcpListener::bind(ADDRESS).unwrap();
 
+    let pool: ThreadPool = ThreadPool::new(THREAD_POOL_SIZE);
+
     for stream in listener.incoming() {
         let stream: TcpStream = stream.unwrap();
-        route(stream);
+
+        pool.execute(|| {
+            handle(stream);
+        });
     }
 }
---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+impl ThreadPool {
+    /// Creates a new ThreadPool
+    ///
+    /// The size is the number of threads in ThreadPool
+    ///
+    /// # Panics
+    ///
+    /// The new fn will panic if the size is 0
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let workers: Vec<Worker> = (0..size)
+            .map(|id| Worker::new(id, Arc::clone(&receiver)))
+            .collect();
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Message::NewJob(Box::new(f));
+        self.sender.send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers");
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting Down all workers");
+        for worker in &mut self.workers {
+            println!("Shutting down worker with id: {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a new job. Executing!", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate. Terminating!", id);
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
